f2d9f9c124f9461016e203272875b4e5bf8f260d
Ivo Araujo <ivo.souza-23@outlook.com>
2024-12-28 19:24:53 -0300

    Iniciando Projeto

---
+++ src/main/java/com/araujosivo/formulaonechampions/FormulaonechampionsApplication.java
@@ -0,0 +1,13 @@
+package com.araujosivo.formulaonechampions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaonechampionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulaonechampionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/araujosivo/formulaonechampions/FormulaonechampionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.araujosivo.formulaonechampions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaonechampionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
