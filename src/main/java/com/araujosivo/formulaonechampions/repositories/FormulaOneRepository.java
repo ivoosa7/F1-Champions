f2370733d0975e06f0e98df2625dade5e7ee46cc
Ivo Araujo <ivo.souza-23@outlook.com>
2025-01-05 23:33:23 -0300

    Acessando o H2

---
+++ src/main/java/com/araujosivo/formulaonechampions/repositories/FormulaOneRepository.java
@@ -0,0 +1,8 @@
+package com.araujosivo.formulaonechampions.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.araujosivo.formulaonechampions.entities.FormulaOne;
+
+public interface FormulaOneRepository extends JpaRepository<FormulaOne, Long>{
+}
